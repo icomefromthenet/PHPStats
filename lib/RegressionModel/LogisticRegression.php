3377322fb0d49d7cb61e5d3990dde1d760497e41
Michael Cordingley <Michael.Cordingley@gmail.com>
2012-01-02 19:29:26 -0500

    Added logistic model.

---
+++ lib/RegressionModel/LogisticRegression.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * PHP Statistics Library
+ *
+ * Copyright (C) 2011-2012 Michael Cordingley<Michael.Cordingley@gmail.com>
+ * 
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * 
+ * LGPL Version 3
+ *
+ * @package PHPStats
+ */
+ 
+namespace PHPStats;
+
+/**
+ * Logistic Regression class
+ * 
+ * Implements the ordinary least squares method of calculating best-fit
+ * regression coefficients.
+ */
+class LogisticRegression extends MultipleRegression {
+	private function transformY($value) {
+		return log($value/(1 - $value));
+	}
+
+	private function reformY($value) {
+		return exp($value)/(1 + exp($value));
+	}
+}
